977bff5e38b72ccf81c1dfb9a43220373e35a48b
Miguel-oc-dev <miguelcontreras56@aragon.unam.mx>
2025-05-23 12:33:07 -0600

    feat: Inicio del curso by  Generation

---
+++ pf-l1-1.0/main.py
@@ -0,0 +1,25 @@
+# Solicitar entradas al usuario
+print("What is your name?")
+name = input()
+
+print("What is your job name?")
+job_name = input()
+
+print("Enter the first adjective:")
+first_adjective = input()
+
+print("Enter the second adjective:")
+second_adjective = input()
+
+print("Enter the first food:")
+first_food = input()
+
+print("Enter the second food:")
+second_food = input()
+
+print("How are you feeling?")
+feeling = input()
+
+# Imprimir el Mad Lib
+print()
+print(f"{name} started their first Generation course today. They are training as a {job_name}. They found their cohort to be very {first_adjective} but their teacher was, at least, {second_adjective}. For lunch they have {first_food} and {second_food} while reviewing their notes. They feel {feeling} but are determined to complete the course.")
---
+++ pf-l2-1.0/main.py
@@ -0,0 +1,14 @@
+shopping_list = []
+
+# Solicitar tres ítems uno por uno
+item1 = input()
+item2 = input()
+item3 = input()
+
+# Agregar ítems a la lista
+shopping_list.append(item1)
+shopping_list.append(item2)
+shopping_list.append(item3)
+
+# Imprimir la lista
+print(shopping_list)
